e48292aa030b2b04ac351c64f66753a9e79fdd85
Dylan Woodcum <dwoodcum1@udayton.edu>
2025-06-25 14:30:31 -0400

    add react component

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { createRoot } from 'react-dom/client';
+//import { createRoot } from 'react-dom/client';
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
